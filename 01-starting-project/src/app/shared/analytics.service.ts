1bcd3d3db71a4f62e0600ec6df931a71b65e924d
Vladimir Stoyanov <vladsto101@gmail.com>
2023-03-23 00:27:52 +0200

    Finished standalone section.

--- 01-starting-project/src/app/shared/analytics.service.ts
+++ 01-starting-project/src/app/shared/analytics.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({ providedIn: 'root' })
+// @Injectable({ providedIn: 'root' }) this is the better option
 export class AnalyticsService {
   registerClick() {
     console.log('Clicked!');
---
+++ 03-starting-with-routing/src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { Route, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const routes: Route[] = [
+  {
+    path: '',
+    component: WelcomeComponent,
+  },
+  {
+    path: 'about',
+    // component: AboutComponent,
+    loadComponent: () => import('./about/about.component')
+      .then((module) => module.AboutComponent)
+  },
+  {
+    path: 'dashboard',
+    // loadChildren: () =>
+    //   import('./dashboard/dashboard-routing.module').then(
+    //     (mod) => mod.DashboardRoutingModule
+    //   ),
+    loadChildren: () =>
+      import('./dashboard/routes').then(
+        (mod) => mod.DASHBOARD_ROUTES
+      )
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
